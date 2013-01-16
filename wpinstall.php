eac9d710ed01f10ccc3f013ca9b4007484e89671
Marko Tomic <marko@markomedia.com.au>
2013-01-17 09:29:24 +1100

    update project name in comments

--- wpinstall.php
+++ wpinstall.php
@@ -7,20 +7,20 @@
  * @copyright Copyright 2012 Marko Tomic
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version 1.0.0
- * This file is part of WPAutoInstall.
+ * This file is part of wordpress-install.
  *
- *   WPAutoInstall is free software: you can redistribute it and/or modify
+ *   wordpress-install is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   WPAutoInstall is distributed in the hope that it will be useful,
+ *   wordpress-install is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with WPAutoInstall.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with wordpress-install.  If not, see <http://www.gnu.org/licenses/>.
  */
 require_once('includes/tools.inc.php');
 $tab = (chr(9));
